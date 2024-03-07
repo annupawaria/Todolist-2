248830f35cf16b3b4cca8913f35279c03b487b88
annupawaria <134483932+annupawaria@users.noreply.github.com>
2024-03-07 15:11:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-// import {uuid} from 'uuid'
-// import { v4 as uuidv4 } from "uuid";
+
 import List from "./components/List/List";
 import "./App.css";
 // import { ListItem } from "@mui/material";
